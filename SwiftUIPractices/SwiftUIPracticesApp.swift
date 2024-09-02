2c1084c9fc9ea393f82bc5f28a7593d84be743c2
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-09-02 13:15:30 +0900

    Initial Commit

---
+++ SwiftUIPractices/SwiftUIPracticesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticesApp.swift
+//  SwiftUIPractices
+//
+//  Created by 조규연 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
